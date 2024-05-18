43431d00b9d16e9e6359c4758690ee721c566847
karolr <rojewskikarol78@gmail.com>
2024-05-18 23:39:52 +0200

    created boiler plate for client side

--- SocialMediaApp.ClientApp/src/app/app.component.ts
+++ SocialMediaApp.ClientApp/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clientapp';
+
 }
--- SocialMediaApp.ClientApp/src/app/app.routes.ts
+++ SocialMediaApp.ClientApp/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './main/login/login.component';
+import { RegisterComponent } from './main/register/register.component';
+import { HomeComponent } from './main/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '**', redirectTo: 'home' }
+];
