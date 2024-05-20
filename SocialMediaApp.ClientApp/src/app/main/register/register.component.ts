7644a5451cc7292e840b29325e7b318ea74d4fbd
karolr <rojewskikarol78@gmail.com>
2024-05-20 15:46:55 +0200

    created register page view

--- SocialMediaApp.ClientApp/src/app/main/register/register.component.ts
+++ SocialMediaApp.ClientApp/src/app/main/register/register.component.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    surname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required)
+  }, { validators: this.passwordMatch(), updateOn: 'blur' });
+
+  private passwordMatch(): ValidatorFn | ValidatorFn[] | null | undefined {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const password = control.get('password');
+      const confirmPassword = control.get('confirmPassword');
+
+      if (password?.value !== confirmPassword?.value) {
+        confirmPassword?.setErrors({ passwordMatch: true });
+      }
+
+      return null;
+    }
+  }
+
+  get name() {
+    return this.registerForm.get('name');
+  }
+
+  get surname() {
+    return this.registerForm.get('surname');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password')
+  }
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.registerForm.value);
+  }
 
 }
